ecb236231f90dc2364778f43531a11e90a58b3b0
ambiennt <63216972+ambiennt@users.noreply.github.com>
2023-11-05 19:24:32 -0800

    Reformat code

--- include/json/autolink.h
+++ include/json/autolink.h
@@ -4,21 +4,21 @@
 // See file LICENSE for detail or copy at http://jsoncpp.sourceforge.net/LICENSE
 
 #ifndef JSONCPP_AUTOLINK_H_INCLUDED
-# define JSONCPP_AUTOLINK_H_INCLUDED
+#define JSONCPP_AUTOLINK_H_INCLUDED
 
-# include "config.h"
+#include "config.h"
 
-# ifdef JSONCPP_IN_CPPTL
-#  include <cpptl/cpptl_autolink.h>
-# endif
+#ifdef JSONCPP_IN_CPPTL
+#include <cpptl/cpptl_autolink.h>
+#endif
 
-# if !defined(JSONCPP_NO_AUTOLINK)  &&  !defined(JSONCPP_DLL_BUILD)  &&  !defined(JSONCPP_IN_CPPTL)
-#  define CPPTL_AUTOLINK_NAME "json"
-#  undef CPPTL_AUTOLINK_DLL
-#  ifdef JSONCPP_DLL
-#   define CPPTL_AUTOLINK_DLL
-#  endif
-#  include "autolink.h"
-# endif
+#if !defined(JSONCPP_NO_AUTOLINK) && !defined(JSONCPP_DLL_BUILD) && !defined(JSONCPP_IN_CPPTL)
+#define CPPTL_AUTOLINK_NAME "json"
+#undef CPPTL_AUTOLINK_DLL
+#ifdef JSONCPP_DLL
+#define CPPTL_AUTOLINK_DLL
+#endif
+#include "autolink.h"
+#endif
 
 #endif // JSONCPP_AUTOLINK_H_INCLUDED
--- include/json/forwards.h
+++ include/json/forwards.h
@@ -4,41 +4,40 @@
 // See file LICENSE for detail or copy at http://jsoncpp.sourceforge.net/LICENSE
 
 #ifndef JSONCPP_FORWARDS_H_INCLUDED
-# define JSONCPP_FORWARDS_H_INCLUDED
+#define JSONCPP_FORWARDS_H_INCLUDED
 
 #if !defined(JSONCPP_IS_AMALGAMATION)
-# include "config.h"
+#include "config.h"
 #endif // if !defined(JSONCPP_IS_AMALGAMATION)
 
 namespace Json {
 
-   // writer.h
-   class FastWriter;
-   class StyledWriter;
+    // writer.h
+    class FastWriter;
+    class StyledWriter;
 
-   // reader.h
-   class Reader;
+    // reader.h
+    class Reader;
 
-   // features.h
-   class Features;
+    // features.h
+    class Features;
 
-   // value.h
-   typedef unsigned int ArrayIndex;
-   class StaticString;
-   class Path;
-   class PathArgument;
-   class Value;
-   class ValueIteratorBase;
-   class ValueIterator;
-   class ValueConstIterator;
+    // value.h
+    typedef unsigned int ArrayIndex;
+    class StaticString;
+    class Path;
+    class PathArgument;
+    class Value;
+    class ValueIteratorBase;
+    class ValueIterator;
+    class ValueConstIterator;
 #ifdef JSONCPP_VALUE_USE_INTERNAL_MAP
-   class ValueMapAllocator;
-   class ValueInternalLink;
-   class ValueInternalArray;
-   class ValueInternalMap;
+    class ValueMapAllocator;
+    class ValueInternalLink;
+    class ValueInternalArray;
+    class ValueInternalMap;
 #endif // #ifdef JSONCPP_VALUE_USE_INTERNAL_MAP
 
 } // namespace Json
-
 
 #endif // JSONCPP_FORWARDS_H_INCLUDED
